6c3527e2ccb072cb9686d48c79f8791797ddcf07
FireElementalNE <FireElemental.NE@gmail.com>
2016-02-02 11:17:21 -0500

    deleted files

---
+++ src/InferenceTransformer.java
@@ -0,0 +1,14 @@
+import soot.Body;
+import soot.BodyTransformer;
+
+import java.util.Map;
+
+/**
+ * Created by fire on 2/2/16.
+ */
+public class InferenceTransformer extends BodyTransformer{
+    @Override
+    protected void internalTransform(Body body, String s, Map<String, String> map) {
+
+    }
+}
