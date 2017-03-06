22f0c8f1966b08f1f4c86e73ee6bde02d4b454b8
FireElementalNE <FireElemental.NE@gmail.com>
2017-03-06 10:46:54 -0500

    removed dumb function

--- src/MainClass.java
+++ src/MainClass.java
@@ -15,34 +15,6 @@ import java.util.zip.ZipFile;
 
 
 public class MainClass {
-    private static String getStringFromInputStream(InputStream is) {
-
-        BufferedReader br = null;
-        StringBuilder sb = new StringBuilder();
-
-        String line;
-        try {
-
-            br = new BufferedReader(new InputStreamReader(is));
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        return sb.toString();
-
-    }
 
     public static void main(String[] args) {
         // TODO: see list
